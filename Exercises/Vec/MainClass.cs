112776ad9575276dd35f79b02ddb6a0e4d8b1543
Esben <esben.ring.moelgaard@gmail.com>
2025-02-10 09:48:10 +0100

    Vec: Implemented and tested approx(Vec, Vec) method

--- Exercises/Vec/MainClass.cs
+++ Exercises/Vec/MainClass.cs
@@ -5,7 +5,11 @@ public static class MainClass {
         Vec A = new Vec(2, 3, 4);
         Console.WriteLine($"A: {A}");
         Vec B = new Vec(-5, 6, 18);
-        Console.WriteLine($"B: {B}\n");
+        Console.WriteLine($"B: {B}");
+        Vec C = new Vec(0.2, -0.0, -13.555);
+        Console.WriteLine($"C: {C}");
+        Vec DefaultVector = new Vec();
+        Console.WriteLine($"Default vector: {DefaultVector}\n");
 
         Console.WriteLine($"A + B: {A + B}");
         Console.WriteLine($"A - B: {A - B}\n");
@@ -14,16 +18,27 @@ public static class MainClass {
         Console.WriteLine($"A * 5: {A * 5}");
         Console.WriteLine($"A / 10: {A / 10}\n");
 
-        Console.WriteLine($"A dot B via static method: {Vec.Dot(A, B)}");
-        Console.WriteLine($"A dot B via instance method: {A.Dot(B)}");
-        Console.WriteLine($"B dot A: {Vec.Dot(B, A)}\n");
+        Console.WriteLine($"A dot B via static method: {Vec.dot(A, B)}");
+        Console.WriteLine($"A dot B via instance method: {A.dot(B)}");
+        Console.WriteLine($"B dot A: {Vec.dot(B, A)}");
+        Console.WriteLine($"A dot A: {Vec.dot(A, A)}\n");
 
-        Console.WriteLine($"A cross B via static method: {Vec.Cross(A, B)}");
-        Console.WriteLine($"A cross B via instance method: {A.Cross(B)}");
-        Console.WriteLine($"B cross A: {Vec.Cross(B, A)}\n");
+        Console.WriteLine($"A cross B via static method: {Vec.cross(A, B)}");
+        Console.WriteLine($"A cross B via instance method: {A.cross(B)}");
+        Console.WriteLine($"B cross A: {Vec.cross(B, A)}\n");
 
-        Console.WriteLine($"A dot (A cross B): {A.Dot(A.Cross(B))}");
-        Console.WriteLine($"A cross (A cross B): {A.Cross(A.Cross(B))}");
-        Console.WriteLine($"A cross (5 * B): {A.Cross(5 * B)}");
+        Console.WriteLine($"A dot (A cross B): {A.dot(A.cross(B))}");
+        Console.WriteLine($"A cross (A cross B): {A.cross(A.cross(B))}");
+        Console.WriteLine($"A cross (5 * B): {A.cross(5 * B)}\n");
+
+        Console.WriteLine($"A == A: {A == A}");
+        Console.WriteLine($"A == B: {A == B}");
+        Console.WriteLine($"A != A: {A != A}");
+        Console.WriteLine($"A != B: {A != B}\n");
+
+        Console.WriteLine($"A approx A: {A.approx(A)}");
+        Console.WriteLine($"A approx B: {A.approx(B)}");
+        Console.WriteLine($"A !approx A: {!A.approx(A)}");
+        Console.WriteLine($"A !approx B: {!A.approx(B)}\n");
     }
 }
--- Exercises/Vec/Vec.cs
+++ Exercises/Vec/Vec.cs
@@ -1,49 +1,69 @@
-public class Vec {
-    public double X { get; }
-    public double Y { get; }
-    public double Z { get; }
+using System;
 
+public class Vec {
+    public double x { get; }
+    public double y { get; }
+    public double z { get; }
+
+    public Vec() { }
     public Vec(double x, double y, double z) {
-        this.X = x;
-        this.Y = y;
-        this.Z = z;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     // Unary plus and minus operators
     public static Vec operator +(Vec a) => a;
-    public static Vec operator -(Vec a) => new Vec(-a.X, -a.Y, -a.Z);
+    public static Vec operator -(Vec a) => new Vec(-a.x, -a.y, -a.z);
 
     // Vector addition and subtraction
-    public static Vec operator +(Vec a, Vec b) => new Vec(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
-    public static Vec operator -(Vec a, Vec b) => new Vec(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+    public static Vec operator +(Vec a, Vec b) => new Vec(a.x + b.x, a.y + b.y, a.z + b.z);
+    public static Vec operator -(Vec a, Vec b) => new Vec(a.x - b.x, a.y - b.y, a.z - b.z);
 
-    // Scalar - Vector multiplication and division
-    public static Vec operator *(double s, Vec a) => new Vec(s * a.X, s * a.Y, s * a.Z);
-    public static Vec operator *(Vec a, double s) => new Vec(s * a.X, s * a.Y, s * a.Z);
-    public static Vec operator /(Vec a, double s) => new Vec(a.X / s, a.Y / s, a.Z / s);
+    // Scalar-Vector multiplication and division
+    public static Vec operator *(double s, Vec a) => new Vec(s * a.x, s * a.y, s * a.z);
+    public static Vec operator *(Vec a, double s) => new Vec(s * a.x, s * a.y, s * a.z);
+    public static Vec operator /(Vec a, double s) => new Vec(a.x / s, a.y / s, a.z / s);
+
+    // Equality
+    public static bool operator ==(Vec a, Vec b) {
+        if (a.x != b.x || a.y != b.y || a.y != b.y) return false;
+        else return true;
+    }
+    public static bool operator !=(Vec a, Vec b) => !(a == b);
+
+    private static bool approx(double a, double b, double acc=1e-9, double eps=1e-9) {
+        double diff = Math.Abs(a - b);
+        if (diff <= acc || diff / Math.Max(Math.Abs(a), Math.Abs(b)) <= eps) { return true; }
+        else { return false; }
+    }
+
+
+    public static bool approx(Vec a, Vec b) {
+        if (!approx(a.x, b.x) || !approx(a.y, b.y) || !approx(a.z, b.z)) { return false; }
+        else { return true; }
+    }
+
+    public bool approx(Vec other) => approx(this, other);
 
     // Dot product
-    public static double Dot(Vec vec1, Vec vec2) {
-        return (vec1.X * vec2.X) + (vec1.Y * vec2.Y) + (vec1.Z * vec2.Z);
+    public static double dot(Vec vec1, Vec vec2) {
+        return (vec1.x * vec2.x) + (vec1.y * vec2.y) + (vec1.z * vec2.z);
     }
 
-    public double Dot(Vec other) {
-        return Dot(this, other);
-    }
+    public double dot(Vec other) => dot(this, other);
 
     // Cross product
-    public static Vec Cross(Vec vec1, Vec vec2) {
-        double crossX = (vec1.Y * vec2.Z) - (vec1.Z * vec2.Y);
-        double crossY = (vec1.Z * vec2.X) - (vec1.X * vec2.Z);
-        double crossZ = (vec1.X * vec2.Y) - (vec1.Y * vec2.X);
+    public static Vec cross(Vec vec1, Vec vec2) {
+        double crossX = (vec1.y * vec2.z) - (vec1.z * vec2.y);
+        double crossY = (vec1.z * vec2.x) - (vec1.x * vec2.z);
+        double crossZ = (vec1.x * vec2.y) - (vec1.y * vec2.x);
         return new Vec(crossX, crossY, crossZ);
     }
 
-    public Vec Cross(Vec other) {
-        return Cross(this, other);
-    }
+    public Vec cross(Vec other) => cross(this, other);
 
     public override string ToString() {
-        return $"({X}, {Y}, {Z})";
+        return $"({x}, {y}, {z})";
     }
 }
